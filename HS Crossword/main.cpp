886de8cac359d87b584eddc73517e4f9eab23c3a
Imali Susan <35591292+imalisusan@users.noreply.github.com>
2022-01-24 18:45:30 +0300

    Update main.cpp

--- HS Crossword/main.cpp
+++ HS Crossword/main.cpp
@@ -29,35 +29,25 @@ void init()
 
 
 void home()
-{
+{   
     //glClear(GL_COLOR_BUFFER_BIT);     // Clear display window
 
-    glClearColor(0, 0, 0, 0);
-    glColor3f(1, 0, 0);
-    glRasterPos3f(250, 350, 0);
-    char msg1[] = "WELCOME TO H.S CROSSWORD";
+	glClearColor(0, 0, 0, 0);
+	glColor3f(1,0,0);
+	glRasterPos3f(300,350,0);
+	char msg1[] = "HS CROSSWORDS";
+	for(int i=0; i<strlen(msg1); i++)
+	{
+	glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, msg1[i]);
+	}
 
-    for (int i = 0; i < strlen(msg1); i++)
-    {
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, msg1[i]);
-    }
-
-    glColor3f(1, 1, 1);
-    glRasterPos3f(300, 300, 0);
-    char inst[] = "Let's test your knowledge";
-
-    for (int i = 0; i < strlen(inst); i++)
-    {
-        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, inst[i]);
-    }
-
-    glColor3f(1, 1, 1);
-    glRasterPos3f(230, 270, 0);
-    char msg2[] = "Press X to start the game, Press Z to exit";
-    for (int i = 0; i < strlen(msg2); i++)
-    {
-        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, msg2[i]);
-    }
+	glColor3f(0,0,1);
+	glRasterPos3f(300,150,0);
+	char msg2[] = "Press X to start the game";
+	for(int i=0; i<strlen(msg2); i++)
+	{
+	glutBitmapCharacter(GLUT_BITMAP_9_BY_15, msg2[i]);
+	}
     // Process all OpenGL routine s as quickly as possible
     glFlush();
 }
@@ -101,7 +91,7 @@ void display()
 
     glColor3f(0,0,0);
     glRasterPos3f(410,480,0);
-    char question3[] = "3. How many colors does a rainbow have?";
+    char question3[] = "3. Changing the direction of a wave results in?";
     //ans= reflux
     for(int l=0; l<strlen(question3); l++)
     {
@@ -110,22 +100,56 @@ void display()
 
     glColor3f(0,0,0);
     glRasterPos3f(410,460,0);
-    char question4[] = "4. Changing the direction of a wave results in?";
-    for(int m=0; m<strlen(question4); m++)
+    char question4[] = "4. Which planet is the nearest to the sun?";
+    for(int n=0; n<strlen(question4); n++)
     {
-        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, question4[m]);
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, question4[n]);
     }
 
     glColor3f(0,0,0);
     glRasterPos3f(410,440,0);
-    char question5[] = "5. Which planet is the nearest to the sun?";
+    char question5[] = "5. The opposite of sine";
     for(int n=0; n<strlen(question5); n++)
     {
         glutBitmapCharacter(GLUT_BITMAP_9_BY_15, question5[n]);
     }
 
+    glColor3f(0,0,0);
+    glRasterPos3f(410,420,0);
+    char question6[] = "6. Transferring pollen from the male anther to female stigma";
+    for(int n=0; n<strlen(question6); n++)
+    {
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, question6[n]);
+    }
 
-        //Draw the first word boxes
+    glColor3f(0,0,0);
+    glRasterPos3f(410,400,0);
+    char question7[] = "7. Relating to living matter";
+    for(int n=0; n<strlen(question7); n++)
+    {
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, question7[n]);
+    }
+
+    glColor3f(0,0,0);
+    glRasterPos3f(410,380,0);
+    char question8[] = "8. The brain of the cell";
+    for(int n=0; n<strlen(question8); n++)
+    {
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, question8[n]);
+    }
+
+    glColor3f(0,0,0);
+    glRasterPos3f(410,360,0);
+    char question9[] = "9. Something to test a theory or a science fact";
+    for(int n=0; n<strlen(question9); n++)
+    {
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, question9[n]);
+    }
+    //Display the label
+    glColor3f(0,0,0);
+    glRasterPos3f(20,535,0);
+    glutBitmapCharacter(GLUT_BITMAP_9_BY_15, '1');
+        //Draw the first word boxes(earth)
         int a = 30;
         int b = 550;
         int c = 50;
@@ -159,7 +183,10 @@ void display()
             c = c+20;
         }  
 
-        //Draw the second word boxes
+        glColor3f(0,0,0);
+        glRasterPos3f(80,515,0);
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, '2');
+        //Draw the second word boxes(saturn)
         int e = 90;
         int f = 530;
         int g = 110;
@@ -191,8 +218,11 @@ void display()
             e = e+20;
             g = g+20;
         }  
-        
-        //Draw the third horizontal word boxes
+
+        glColor3f(0,0,0);
+        glRasterPos3f(175,535,0);
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, '3');
+        //Draw the third horizontal word boxes(reflux)
         int i = 170;
         int j = 530;
         int k = 190;
@@ -205,6 +235,7 @@ void display()
             glVertex2i(k, j);
             glVertex2i(k, l);
             glVertex2i(i, l);
+
             glEnd();
 
              //lines of squere
@@ -224,13 +255,16 @@ void display()
             j = j-20;
             l = l-20;
         }  
-		
-         //Draw the fourth word boxes
-        int m = 50;
+
+        glColor3f(0,0,0);
+        glRasterPos3f(80,435,0);
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, '4');
+          //Draw the fourth word boxes(mercury)
+        int m = 90;
         int n = 450;
-        int o = 70;
+        int o = 110;
         int p = 430;
-        for (int i = 0; i < 8; i++) 
+        for (int i = 0; i < 7; i++) 
         {
             glColor3f(1, 1, 1);
             glBegin(GL_POLYGON);
@@ -258,19 +292,58 @@ void display()
             o = o+20;
         }  
 
-        //Draw the fifth horizontal word boxes
-        int q = 110;
-        int r = 430;
-        int s = 130;
-        int t = 410;
-        for (int ar = 0; ar < 4; ar++) 
+        glColor3f(0,0,0);
+        glRasterPos3f(155,455,0);
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, '5');
+        //Draw the fifth word boxes(cosine)
+        int mm = 150;
+        int nn = 430;
+        int oo = 170;
+        int pp = 410;
+        for (int ii = 0; ii < 5; ii++) 
+        {
+            glColor3f(1, 1, 1);
+            glBegin(GL_POLYGON);
+            glVertex2i(mm, nn);
+            glVertex2i(oo, nn);
+            glVertex2i(oo, pp);
+            glVertex2i(mm, pp);
+            glEnd();
+
+             //lines of square
+            glColor3f(0, 0, 0);
+            glLineWidth(1);
+            glBegin(GL_LINES);
+            glVertex2i(mm, nn); 
+            glVertex2i(oo, nn); 
+            glVertex2i(oo, nn);
+            glVertex2i(oo, pp);
+            glVertex2i(oo, pp);                                               
+            glVertex2i(mm, pp);
+            glVertex2i(mm, pp); 
+            glVertex2i(mm, nn);
+            glEnd();
+
+            nn = nn-20;
+            pp = pp-20;
+        }  
+        
+        glColor3f(0,0,0);
+        glRasterPos3f(55,415,0);
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, '6');
+        //Draw the sixth horizontal word boxes (pollinate)
+        int q = 50;
+        int r = 410;
+        int s = 70;
+        int t = 390;
+        for (int ar = 0; ar < 9; ar++) 
         {
             glColor3f(1, 1, 1);
             glBegin(GL_POLYGON);
             glVertex2i(q, r);
             glVertex2i(s, r);
-            glVertex2i(s, l);
-            glVertex2i(q, l);
+            glVertex2i(s, t);
+            glVertex2i(q, t);
             glEnd();
 
              //lines of squere
@@ -291,11 +364,14 @@ void display()
             t = t-20;
         }  
 
-         //Draw the sixth word boxes
-        int u = 50;
-        int v = 370;
-        int w = 70;
-        int x = 350;
+        glColor3f(0,0,0);
+        glRasterPos3f(35,375,0);
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, '7');
+         //Draw the seventh word boxes(organic)
+        int u = 70;
+        int v = 390;
+        int w = 90;
+        int x = 370;
         for (int i = 0; i < 6; i++) 
         {
             glColor3f(1, 1, 1);
@@ -323,7 +399,79 @@ void display()
             u = u+20;
             w = w+20;
         }  
+
+        glColor3f(0,0,0);
+        glRasterPos3f(35,295,0);
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, '8');
+         //Draw the eigth word boxes(nucleus)
+        int aa = 70;
+        int ab = 310;
+        int ac = 90;
+        int ad = 290;
+        for (int i = 0; i < 6; i++) 
+        {
+            glColor3f(1, 1, 1);
+            glBegin(GL_POLYGON);
+            glVertex2i(aa, ab);
+            glVertex2i(ac, ab);
+            glVertex2i(ac, ad);
+            glVertex2i(aa, ad);
+            glEnd();
+
+             //lines of squere
+            glColor3f(0, 0, 0);
+            glLineWidth(1);
+            glBegin(GL_LINES);
+            glVertex2i(aa, ab); 
+            glVertex2i(ac, ab); 
+            glVertex2i(ac, ab);
+            glVertex2i(ac, ad);
+            glVertex2i(ac, ad);                                               
+            glVertex2i(aa, ad);
+            glVertex2i(aa, ad); 
+            glVertex2i(aa, ab);
+            glEnd();
+
+            aa = aa+20;
+            ac = ac+20;
+        } 
         
+        glColor3f(0,0,0);
+        glRasterPos3f(35,235,0);
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, '9');
+         //Draw the nineth word boxes(experiment)
+        int ba = 70;
+        int bb = 250;
+        int bc = 90;
+        int bd = 230;
+        for (int i = 0; i < 9; i++) 
+        {
+            glColor3f(1, 1, 1);
+            glBegin(GL_POLYGON);
+            glVertex2i(ba, bb);
+            glVertex2i(bc, bb);
+            glVertex2i(bc, bd);
+            glVertex2i(ba, bd);
+            glEnd();
+
+             //lines of squere
+            glColor3f(0, 0, 0);
+            glLineWidth(1);
+            glBegin(GL_LINES);
+            glVertex2i(ba, bb); 
+            glVertex2i(bc, bb); 
+            glVertex2i(bc, bb);
+            glVertex2i(bc, bd);
+            glVertex2i(bc, bd);                                               
+            glVertex2i(ba, bd);
+            glVertex2i(ba, bd); 
+            glVertex2i(ba, bb);
+            glEnd();
+
+            ba = ba+20;
+            bc = bc+20;
+        } 
+
 		glColor3f(0,0,1);
 		glRasterPos3f(650,20,0);
 		char msg2[] = "Press Z to exit";
@@ -338,6 +486,11 @@ void display()
 void keys(unsigned char key, int x, int y)
 {
     if(key == 'X')
+    {
+        glutDisplayFunc(display);
+    }
+
+    if(key == 'x')
     {
         glutDisplayFunc(display);
     }
