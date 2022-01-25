0ce6526c2b2c53184f77c9c4756fab61eadceab0
Victor Mutuku <71630718+victormutuku@users.noreply.github.com>
2022-01-26 00:03:48 +0300

    Game over file added

---
+++ HS Crossword/gameover.cpp
@@ -0,0 +1,63 @@
+// for MS Windows
+//#include<Windows.h>   
+#include <GL/glut.h>      
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <iostream>
+#include <string.h>
+#include <fstream>
+
+using namespace std;
+//Note: GLglut.h path depending on the system in use  
+//Linux compile: 
+
+void addLetter();
+void gameOverKeys(unsigned char key, int x, int y)
+{
+
+	if(key == 'Z')
+    {
+        glutHideWindow();
+    }
+ 
+   
+    glutPostRedisplay();
+}
+
+void gameOver()
+{
+	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+  
+   	glClearColor(0, 0, 0, 0);
+	glColor3f(1,0,0);
+	glRasterPos3f(300,350,0);
+	char msg1[] = "GAME OVER!!!";
+	for(int i=0; i<strlen(msg1); i++)
+	{
+	glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, msg1[i]);
+	}
+
+	glColor3f(0,0,1);
+	glRasterPos3f(300,150,0);
+	char msg2[] = "You won. No next level";
+	for(int i=0; i<strlen(msg2); i++)
+	{
+	glutBitmapCharacter(GLUT_BITMAP_9_BY_15, msg2[i]);
+	}
+
+   	glColor3f(0,0,1);
+		glRasterPos3f(650,20,0);
+		char msg3[] = "Press Z to exit";
+		for(int i=0; i<strlen(msg3); i++)
+		{
+			glutBitmapCharacter(GLUT_BITMAP_9_BY_15, msg3[i]);
+		}
+    glutPostRedisplay();
+    glutSwapBuffers();
+    glutKeyboardFunc(gameOverKeys);
+}
+
+
+
+
